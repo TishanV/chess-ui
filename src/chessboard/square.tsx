23cc3ed0a60df873f367398ef1ce08947d9e1fd4
Tis <tishan@protonmail.com>
2022-07-03 21:49:57 +0530

    --feat: piece draggable

--- src/chessboard/square.tsx
+++ src/chessboard/square.tsx
@@ -1,74 +1,45 @@
 import React from "react"
-import { MouseEvent, useRef, RefObject } from "react";
 import { BoardColor } from "../globals";
 import { appSize } from "../store";
-import { useRecoilValue } from "recoil";
-import { square } from "../store/chessboard";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { square, placePieces } from "../store/chessboard";
+import { Draggable } from "../components/draggable";
+import { useRef } from "react";
 
 interface SquareProps {
   id: number;
 }
 
 function Square(props: SquareProps) {
-  const pieceRef = useRef<HTMLImageElement>(null);
   const piece = useRecoilValue(square(props.id));
+  const movePiece = useSetRecoilState(placePieces);
   const size = useRecoilValue(appSize) / 8;
+  const squareRef = useRef<HTMLDivElement>(null);
+  const boardRect = squareRef.current?.parentElement?.getBoundingClientRect();
   console.log(`Square ${props.id} render`);
   return (
     <div
+      ref={squareRef}
+      id={`chess-ui-square-${props.id}`}
       className="square"
       style={{ backgroundColor: BoardColor[paritySq(props.id)] }}
     >
       {piece != " " ? (
-        <img
-          ref={pieceRef}
-          className="piece"
-          src={piecePath(piece)}
-          alt="piece"
-          width={size * 0.95}
-          onMouseDown={pieceHold}
-          onMouseMove={(e) => pieceMove(e, pieceRef)}
-          onMouseUp={(e) => pieceRelease(e, pieceRef)}
-        />
+        <Draggable
+          boundTop={boardRect?.top}
+          boundBottom={boardRect?.bottom}
+          boundLeft={boardRect?.left}
+          boundRight={boardRect?.right}
+          onDragEnd={(e) =>
+            movePiece(moveAction(piece, props.id, e.clientX, e.clientY))
+          }
+        >
+          <img src={piecePath(piece)} alt="piece" width={size * 0.95} />
+        </Draggable>
       ) : null}
     </div>
   );
 }
-//
-let dx = 0;
-let dy = 0;
-let boardY = 0;
-let boardX = 0;
-let drag = false;
-//React.MouseEventHandler<HTMLImageElement>
-const pieceHold = (e: MouseEvent) => {
-  e.preventDefault();
-  dx = e.clientX - e.currentTarget.getBoundingClientRect().left;
-  dy = e.clientY - e.currentTarget.getBoundingClientRect().top;
-  boardY =
-    e.currentTarget.parentElement?.parentElement?.getBoundingClientRect().top ??
-    0;
-  boardX =
-    e.currentTarget.parentElement?.parentElement?.getBoundingClientRect()
-      .left ?? 0;
-  drag = true;
-};
-
-const pieceMove = (e: MouseEvent, piece: RefObject<HTMLImageElement>) => {
-  e.preventDefault();
-  if (drag && piece.current) {
-    piece.current.style.left = (e.clientX - boardX - dx).toString() + "px";
-    piece.current.style.top = (e.clientY - boardY - dy).toString() + "px";
-  }
-};
-
-const pieceRelease = (e: MouseEvent, piece: RefObject<HTMLImageElement>) => {
-  drag = false;
-  if (piece.current) {
-    piece.current.style.left = "";
-    piece.current.style.top = "";
-  }
-};
 
 // HELPER FUNCTIONS
 function paritySq(id: number) {
@@ -77,6 +48,21 @@ function paritySq(id: number) {
 
 function piecePath(piece: string) {
   return "../../assets/pieces/" + piece + ".svg";
+}
+
+function squareIDFromCords(x: number, y: number) {
+  const divId = document
+    .elementsFromPoint(x, y)
+    .find((e) => e.className == "square")?.id;
+  if (divId?.slice(0, 15) === "chess-ui-square") return +divId.slice(16);
+}
+
+// AVOID DIRECT MANIPULATION
+function moveAction(piece: string, fromID: number, x: number, y: number) {
+  const toID = squareIDFromCords(x, y);
+  if (toID === fromID) return [];
+  if (toID !== undefined) return [`${fromID} `, `${toID}${piece}`];
+  return [];
 }
 
 export { Square };
---
+++ src/components/draggable.tsx
@@ -0,0 +1,73 @@
+import React, { useRef } from "react";
+
+interface BoundProps {
+  boundTop?: number;
+  boundBottom?: number;
+  boundRight?: number;
+  boundLeft?: number;
+}
+
+interface DragEvents {
+  onDragStart?: (event: React.MouseEvent | MouseEvent) => any;
+  onDragEnd?: (event: React.MouseEvent | MouseEvent) => any;
+}
+
+interface DraggableProps extends BoundProps, DragEvents {
+  children: JSX.Element;
+}
+
+function Draggable(props: DraggableProps) {
+  const dragContainerRef = useRef<HTMLDivElement>(null);
+
+  const startDrag = (event: React.MouseEvent) => {
+    event.preventDefault();
+    const boxTop = event.currentTarget.getBoundingClientRect().top;
+    const boxLeft = event.currentTarget.getBoundingClientRect().left;
+    const boxBottom = event.currentTarget.getBoundingClientRect().bottom;
+    const boxRight = event.currentTarget.getBoundingClientRect().right;
+    console.log(boxTop, boxLeft, boxBottom, boxRight);
+    const onMoveEvent = (e: MouseEvent) => {
+      e.preventDefault();
+      if (dragContainerRef.current) {
+        const dx =
+          e.clientX + boxLeft - event.clientX < props.boundLeft!
+            ? props.boundLeft! - boxLeft
+            : e.clientX + boxRight - event.clientX > props.boundRight!
+            ? props.boundRight! - boxRight
+            : e.clientX - event.clientX;
+        const dy =
+          e.clientY + boxTop - event.clientY < props.boundTop!
+            ? props.boundTop! - boxTop
+            : e.clientY + boxBottom - event.clientY > props.boundBottom!
+            ? props.boundBottom! - boxBottom
+            : e.clientY - event.clientY;
+        dragContainerRef.current.style.transform = `translate(${dx}px, ${dy}px)`;
+      }
+    };
+    document.addEventListener("mousemove", onMoveEvent);
+    document.addEventListener(
+      "mouseup",
+      (e) => {
+        document.removeEventListener("mousemove", onMoveEvent);
+        e.preventDefault();
+        if (dragContainerRef.current) {
+          dragContainerRef.current.style.transform = "";
+        }
+        if (props.onDragEnd) props.onDragEnd(e);
+      },
+      { once: true }
+    );
+    if (props.onDragStart) props.onDragStart(event);
+  };
+  return (
+    <div
+      ref={dragContainerRef}
+      onMouseDown={startDrag}
+      style={{ position: "absolute" }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export { Draggable };
